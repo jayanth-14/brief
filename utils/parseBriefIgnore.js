50b17eef97ae6b6a293f3019120dd9bbb2d7b2b8
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-23 10:20:21 +0530

    added checks for comments and empty lines in parsing briefignore

--- utils/parseBriefIgnore.js
+++ utils/parseBriefIgnore.js
@@ -1,8 +1,14 @@
+const isAComment = line => line.startsWith('#');
+const isEmptyLine = line => line.trim().length === 0;
+
 export const parseBriefIgnore = (filePath) => {
   const files = Deno.readTextFileSync(filePath).split('\n');
   const ignoreList = [];
 
   for (const file of files) {
+    // handling comments and empty lines
+    if (isAComment(file) || isEmptyLine(file)) continue;
+
     let fileName = file;
     if (fileName.startsWith('-r')) {
       fileName = fileName.split(' ')[1];
